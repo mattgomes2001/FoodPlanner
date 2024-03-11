e133c23ca07059a8cae6826fee8459265312d8df
Matthieu <Matthieu>
2024-03-11 19:22:10 +0100

    refonte des classes bouffe + methode toMap et toJson sur chaques class

--- lib/data_classes.dart
+++ lib/data_classes.dart
@@ -1,36 +1,66 @@
-class Aliment {
+import 'dart:convert';
+
+class Nourriture {
   // Nom, Quantite
   final String name;
-  final double quantity;
 
-  Aliment(this.name, this.quantity);
+  Nourriture(this.name);
+
+  Map toMap() {
+    return {"name": name};
+  }
+
+  String toJson() {
+    return json.encode(toMap());
+  }
 }
 
-class Ingredient extends Aliment {
+class Ingredient extends Nourriture {
   // Nom, Quantite, CaloriePerUnite, Is
+  final double quantity;
   final int caloriePerUnit;
   final bool isDenombrable;
   final String unite;
 
   Ingredient(
-      String name, double quantity, this.isDenombrable, this.caloriePerUnit)
+      String name, this.quantity, this.isDenombrable, this.caloriePerUnit)
       : unite = isDenombrable ? "" : "g",
-        super(name, quantity);
+        super(name);
+
+  @override
+  Map toMap() {
+    var map = super.toMap();
+    map.addAll({
+      'quantity': quantity,
+      'caloriePerUnit': caloriePerUnit,
+      'isDenombrable': isDenombrable,
+      'unite': unite
+    });
+    return map;
+  }
 }
 
-class PlatPrepare extends Aliment {
-  PlatPrepare(String name, double quantity) : super(name, quantity);
+class PlatPrepare extends Nourriture {
+  final double quantity;
+  PlatPrepare(String name, this.quantity) : super(name);
+
+  @override
+  Map toMap() {
+    var map = super.toMap();
+    map.addAll({'quantity': quantity});
+    return map;
+  }
 }
 
-class Recette {
-  final String name;
+class Recette extends Nourriture {
   final int duration;
   final List<Ingredient> listIng;
-  final int calories;
   final String pathToRecipe;
+  final int calories;
 
-  Recette(this.name, this.duration, this.listIng, this.pathToRecipe)
-      : calories = _calculateCalories(listIng);
+  Recette(String name, this.duration, this.listIng, this.pathToRecipe)
+      : calories = _calculateCalories(listIng),
+        super(name);
 
   static int _calculateCalories(List<Ingredient> listIng) {
     int totalCalories = 0;
@@ -39,5 +69,22 @@ class Recette {
           (ingredient.quantity * ingredient.caloriePerUnit).round();
     }
     return totalCalories;
+  }
+
+  @override
+  Map toMap() {
+    var map = super.toMap();
+    var listIngMaps = [];
+    for (var i = 0; i < listIng.length; i++) {
+      listIngMaps.add(listIng[i].toMap());
+    }
+    map.addAll({
+      "duration": duration,
+      "listIng": listIngMaps,
+      "pathToRecipe": pathToRecipe,
+      'calories': calories
+    });
+
+    return map;
   }
 }
